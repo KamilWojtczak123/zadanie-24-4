ccecd7cd2e4189de564d2f94d4485de8c6cc47e4
KamilWojtczak123 <35527613+KamilWojtczak123@users.noreply.github.com>
2018-05-26 15:55:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,18 @@
 const formatDate = (timeInSeconds) => {
-    let s = '${timeInSeconds}s';
-    let m = 0;
-    let h = 0;
+    if(!timeInSeconds) {
+        return '0s';
+    }
     
-    if(s<60) {
-        return s +'s';
-    }
-    else if(s< 3600) {
-        m = Math.floor(s/60);
-	newtime = s - 60*m;
-	if (newtime === 0) {
-	  return m+'m'; 
-	}
-	else {
-      return m+'m '+newtime+'s';
-    }
-  }
-  else if (s >= 3600) {
-    h = Math.floor(s/3600);
-    m = Math.floor((s - 3600*h)/60);
-    newtime = s - (3600*h+ 60*m);
-    if (newtime === 0 && m === 0) {
-      return h + 'h';
-    }
-    else if (newtime === 0) {
-      return h + 'h ' + m+ 'm';
-    }
-    else if (m === 0) {
-      return h + 'h ' + newtime+ 's';
-    }
-    else {
-      return h + 'h ' + m+ 'm '+ newtime + 's';
-    }
-  }
-  else {
-    return '0s';
-  }
-    }
+    const sec_num = parseInt(timeInSeconds, 10);
+    const hours = Math.floor(sec_num / 3600) % 24;
+    const minutes = Math.floor(sec_num / 60) % 60;
+    const seconds = sec_num % 60;
+    
+    return [
+        hours ? hours + 'h ' : '',
+        minutes ? minutes + 'm ' : '',
+        seconds ? seconds + 's ' : ''
+    ].join('').trim();
+}
 
 module.exports = formatDate;
